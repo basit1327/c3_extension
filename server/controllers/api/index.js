c19cbe1e452ea365772e7299cf5918370d6c0791
basit <razabasit88@gmail.com>
2021-01-08 15:34:09 +0500

    logout api

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -2,10 +2,12 @@
 
 const
 	express = require('express'),
+	userAuthService = require('../../services/user/user_authentication'),
 	userProfileService = require('../../services/user/user_profile');
 
 let router = express.Router();
 
+router.get('/logout', userAuthService.userLogOut);
 router.post('/update', userProfileService.updateAccount);
 router.get('/delete_own', userProfileService.deleteOwnAccount);
 
