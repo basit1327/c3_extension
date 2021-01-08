2ee94db1691bf1f4a4bcb28afc1cd6d22e1dae1d
basit <razabasit88@gmail.com>
2021-01-08 15:38:27 +0500

    postman collection and new database

--- c3.sql
+++ c3.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2020 at 08:50 AM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Generation Time: Jan 08, 2021 at 11:37 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,19 +32,18 @@ CREATE TABLE `accounts` (
   `hash` varchar(45) DEFAULT NULL,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
-  `user_name` varchar(45) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   `password` varchar(45) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0 mean account deleted\n1 mean active'
+  `status` tinyint(1) DEFAULT 1 COMMENT '0 mean account deleted\n1 mean active'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `hash`, `first_name`, `last_name`, `user_name`, `password`, `status`) VALUES
-(4, '63d15926e036a863629ccdb5ade0ca95', 'Basit', 'Raza', 'basitraza', '827ccb0eea8a706c4c34a16891f84e7b', 1),
-(6, 'e0ccebcd9595156f6259cea9afad285d', 'aa', 'bb', 'basitraza3', '827ccb0eea8a706c4c34a16891f84e7b', 1),
-(7, '235871cf93aef59e3e59baa61d8c1cd7', 'aa', 'bb', 'basitraza4', '827ccb0eea8a706c4c34a16891f84e7b', 1);
+INSERT INTO `accounts` (`id`, `hash`, `first_name`, `last_name`, `email`, `password`, `status`) VALUES
+(4, '63d15926e036a863629ccdb5ade0ca95', 'Basit', 'Raza', 'basit@mail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1),
+(9, '27294ecb15375a1e0068aa2b13fa4458', 'Basit', 'R', 'basit2@mail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1);
 
 -- --------------------------------------------------------
 
@@ -58,7 +56,7 @@ CREATE TABLE `user_sessions` (
   `hash` varchar(45) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `created_at` varchar(15) DEFAULT NULL COMMENT 'timestamp at what the session is created',
-  `status` tinyint(1) DEFAULT '1' COMMENT '0 mean deleted\n1 mean active'
+  `status` tinyint(1) DEFAULT 1 COMMENT '0 mean deleted\n1 mean active'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -66,7 +64,13 @@ CREATE TABLE `user_sessions` (
 --
 
 INSERT INTO `user_sessions` (`id`, `hash`, `user_id`, `created_at`, `status`) VALUES
-(6, 'e09ee59ed0bea7a62681b4f16fb97d9e', 4, '1582096935610', 1);
+(6, 'e09ee59ed0bea7a62681b4f16fb97d9e', 4, '1582096935610', 1),
+(7, '261bd57e9e1867c73f5f7fcfe1a1cb3b', 4, '1610101087816', 1),
+(8, '89ee1afd847c51ef0c026a89332d41f9', 4, '1610101358566', 1),
+(9, 'adffac8df2cfca100f838f11d2f3e580', 4, '1610101360331', 1),
+(10, 'b16311a254cfd7255a14be6fc5903460', 9, '1610101481944', 0),
+(11, '40008123b9d61a0dd23a222b4f46fa42', 9, '1610102093648', 1),
+(12, 'defe6c3ecea4cbd5d2cd5ab35950aee7', 9, '1610102120896', 1);
 
 --
 -- Indexes for dumped tables
@@ -77,7 +81,7 @@ INSERT INTO `user_sessions` (`id`, `hash`, `user_id`, `created_at`, `status`) VA
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_name` (`user_name`);
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `user_sessions`
@@ -95,13 +99,13 @@ ALTER TABLE `user_sessions`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_sessions`
 --
 ALTER TABLE `user_sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
